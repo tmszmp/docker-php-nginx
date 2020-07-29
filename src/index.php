1305424f703189b2565bd136ef7be810e63e05f6
Tim Schatz <tischa@medipolis.intern>
2020-07-29 07:58:58 +0200

    29.07.2020

--- src/index.php
+++ src/index.php
@@ -1,3 +1,15 @@
 <?php
 echo "test";
-?>
+$servername = "10.7.252.12";
+$username = "root";
+$password = "test";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?> 
