65bf2d699063d0387254bd66739d1e76cf23bc6d
Tim Schatz <tischa@medipolis.intern>
2020-07-30 13:00:19 +0200

    30.07.2020

--- src/index.php
+++ src/index.php
@@ -22,8 +22,7 @@ $result = $stmt->get_result();
 while($row = $result->fetch_assoc()) {
    echo json_encode($row);
 }*/
-	header('Access-Control-Allow-Origin: *');
-	header('Content-Type: application/json');
-	$response = json_decode(http_get("https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=439d4b804bc8187953eb36d2a8c26a02"));
+	echo http_get("https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=439d4b804bc8187953eb36d2a8c26a02");
+	$response = array(json_decode(http_get("https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=439d4b804bc8187953eb36d2a8c26a02")));
 	echo json_encode($response);
 ?> 
