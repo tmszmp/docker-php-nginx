01fee67723abf8d45f0017014b7d211f2b00e08b
Tim Schatz <tischa@medipolis.intern>
2020-07-30 08:36:13 +0200

    30.07.2020

--- src/index.php
+++ src/index.php
@@ -1,21 +1,12 @@
 <?php
-$pdo = new PDO('mysql:host=10.7.252.12;dbname=cities', 'root', 'test');
- 
-$sql = "SELECT * FROM cities LIMIT 0,10";
-foreach ($pdo->query($sql) as $row) {
-   echo $row['ID']."<br />";
-   echo $row['geo_point']."<br />";
-   echo $row['name']."<br />";
-   echo $row['plz']."<br /><br />";
-}
-
-/*echo "test4";
+echo "test5";
 $servername = "10.7.252.12";
 $username = "root";
 $password = "test";
+$db = 'cities';
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $db);
 
 // Check connection
 if ($conn->connect_error) {
@@ -23,5 +14,11 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 echo "Connected successfully";
-*/
+
+$query = 'SELECT * FROM ' . $this->table . 'ORDER BY name LIMIT 0,10';
+$stmt = $conn->prepare($query);
+$stmt->execute();
+while($row = $statement->fetch()) {
+   echo json_encode($row);
+}
 ?> 
