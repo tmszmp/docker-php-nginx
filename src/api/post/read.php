b62ae11b581d2547e9db19f4be304d0b5604222b
Tim Schatz <tischa@medipolis.intern>
2020-07-30 11:25:37 +0200

    30.07.2020

--- src/api/post/read.php
+++ src/api/post/read.php
@@ -12,16 +12,19 @@
 
 	$stmt = $post->read();
 	$result = $stmt->get_result();
+	while($row = $result->fetch_assoc()) {
+	   echo json_encode($row);
+	}
 	//$num = $result->rowCount();
 
 	//if($num > 0){
-		$posts_arr = array();
+		/*$posts_arr = array();
 		$posts_arr['data'] = array();
 		while($row = $result->fetch_assoc()) {
 		   $post_item = array( 'id' => $row['ID'], 'geo_point' => $row['geo_point'], 'name' => $row['name'], 'plz' => $row['plz']);
 		   array_push($posts_arr['data'], $post_item);
 		}
-		echo json_encode($posts_arr);
+		echo json_encode($posts_arr);*/
 	//}else{
 //		echo json_encode(array('message' => 'Nothing Found'));
 //	}
