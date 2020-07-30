faab37c1b1e39791a1e10a1121d6e5ff66e8262a
Tim Schatz <tischa@medipolis.intern>
2020-07-30 12:47:31 +0200

    30.07.2020

--- src/api/post/read.php
+++ src/api/post/read.php
@@ -1,26 +1,21 @@
 <?php
 	header('Access-Control-Allow-Origin: *');
 	header('Content-Type: application/json');
+	$plz = $_POST['plz'];
 	include_once '../../config/Database.php';
 	include_once '../../models/Post.php';
 
 	$database = new Database();
 	$db = $database->connect();
 	$post = new Post($db);
-	$stmt = $post->read();
+	$stmt = $post->read($plz);
 	$result = $stmt->get_result();
-	//$num = $result->rowCount();
-
-	//if($num > 0){
-		$posts_arr = array();
-		$posts_arr['data'] = array();
-		while($row = $result->fetch_assoc()) {
-		   $post_item = array( 'id' => $row['ID'], 'geo_point' => $row['geo_point'], 'name' => $row['name'], 'plz' => $row['plz']);
-		   array_push($posts_arr['data'], $post_item);
-		}
-		echo json_encode($posts_arr);
-	//}else{
-//		echo json_encode(array('message' => 'Nothing Found'));
-//	}
+	$posts_arr = array();
+	$posts_arr['data'] = array();
+	while($row = $result->fetch_assoc()) {
+	   $post_item = array( 'id' => $row['ID'], 'geo_point' => $row['geo_point'], 'name' => $row['name'], 'plz' => $row['plz']);
+	array_push($posts_arr['data'], $post_item);
+	}
+	echo json_encode($posts_arr);
 
 ?>
--- src/models/Post.php
+++ src/models/Post.php
@@ -7,9 +7,10 @@
 			$this->conn = $db;
 		}
 
-		public function read(){
-			$query = 'SELECT * FROM cities WHERE 1 ORDER BY name LIMIT 0,10';
+		public function read($plz){
+			$query = 'SELECT * FROM cities WHERE plz LIKE ? ORDER BY name LIMIT 0,10';
 			$stmt = $this->conn->prepare($query);
+			$stmt->bind_param('s', $plz);
 			$stmt->execute();
 			return $stmt;
 		}
