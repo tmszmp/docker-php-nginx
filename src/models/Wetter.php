2e2b0ad8d5dd8b9f34acb55d4ccf7cac8a586db7
Tim Schatz <tischa@medipolis.intern>
2020-07-30 14:04:16 +0200

    30/07/2020

--- src/models/Wetter.php
+++ src/models/Wetter.php
@@ -1,10 +1,10 @@
 <?php
 	class Wetter {
-		private $url = 'samples.openweathermap.org/data/2.5/weather';
+		private $url = 'http://samples.openweathermap.org/data/2.5/weather';
 		private $key = '439d4b804bc8187953eb36d2a8c26a02';
 
 		public function read($lat, $lon){
-			$file = $url . "?lat=" . $lat . "&lon=" . $lon . "&appid="$key;
+			$file = $url . "?lat=" . $lat . "&lon=" . $lon . "&appid=" . $key;
 			$data = file_get_contents($file);
 			$result = json_decode($data, true);
 			return $result;
