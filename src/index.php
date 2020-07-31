21c3ab042d6457841cbb7a23267d7d87c09d4e7e
Tim Schatz <tischa@medipolis.intern>
2020-07-31 10:25:20 +0200

    31/07/2020

--- src/index.php
+++ src/index.php
@@ -7,9 +7,11 @@
 
 	$methode = $_SERVER['REQUEST_METHOD'];
 	$req = explode('/', trim($_SERVER['REQUEST_URI'],'/'));
+	echo var_dump($req);
 	switch($req[1]){
 		case 'plz':
 			$plz = $req[2];
+			break;
 	}
 	switch ($methode) {
 	  case 'GET':
